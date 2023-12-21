e84dbb30bb0f1e987037c28ac94711b8d456a588
Carlos D Montilla <94148308+balmond3@users.noreply.github.com>
2023-12-20 19:29:25 -0700

    modified:   index.html 	modified:   script.js 	modified:   styles.css
    actvitated the button next step  and  go back
    button

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let currentStep = 1;
 
+document.getElementById('nextBtn1').addEventListener('click', () => nextStep());
+document.getElementById('prevBtn2').addEventListener('click', () => previousStep(1));
+document.getElementById('nextBtn2').addEventListener('click', () => nextStep());
+document.getElementById('prevBtn3').addEventListener('click', () => previousStep(2));
+document.getElementById('nextBtn3').addEventListener('click', () => nextStep());
+document.getElementById('prevBtn4').addEventListener('click', () => previousStep(3));
+document.getElementById('submitBtn').addEventListener('click', () => submitForm());
+
+function nextStep() {
+  document.getElementById('step' + currentStep).style.display = 'none';
+  currentStep++;
+  document.getElementById('step' + currentStep).style.display = 'block';
+}
+
+function previousStep(targetStep) {
+  document.getElementById('step' + currentStep).style.display = 'none';
+  currentStep = targetStep;
+  document.getElementById('step' + currentStep).style.display = 'block';
+}
