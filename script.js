2c70ea412714cd7fbe98456c85e7f06e6d6302e3
Carlos D Montilla <94148308+balmond3@users.noreply.github.com>
2023-12-24 00:53:46 -0700

    modified:   index.html 	modified:   script.js 	modified:   styles.css
    added the side bar circle steps functionality when buttons are pressed

--- script.js
+++ script.js
@@ -6,16 +6,34 @@ document.getElementById('nextBtn2').addEventListener('click', () => nextStep());
 document.getElementById('prevBtn3').addEventListener('click', () => previousStep(2));
 document.getElementById('nextBtn3').addEventListener('click', () => nextStep());
 document.getElementById('prevBtn4').addEventListener('click', () => previousStep(3));
-document.getElementById('submitBtn').addEventListener('click', () => submitForm());
+// document.getElementById('submitBtn').addEventListener('click', () => submitForm());
+
+
 
 function nextStep() {
   document.getElementById('step' + currentStep).style.display = 'none';
+  document.getElementById('circle' + currentStep).style.backgroundColor = 'transparent';
+  document.getElementById('circle' + currentStep).style.color = '#FFF';
   currentStep++;
   document.getElementById('step' + currentStep).style.display = 'block';
+  document.getElementById('circle' + currentStep).style.backgroundColor = 'hsl(228, 100%, 84%)';
+  document.getElementById('circle' + currentStep).style.color = 'hsl(213, 96%, 18%)';
+  
+
+
 }
 
 function previousStep(targetStep) {
   document.getElementById('step' + currentStep).style.display = 'none';
+  document.getElementById('circle' + currentStep).style.backgroundColor = 'transparent';
+  document.getElementById('circle' + currentStep).style.color = '#fff';
   currentStep = targetStep;
   document.getElementById('step' + currentStep).style.display = 'block';
+  document.getElementById('circle' + currentStep).style.backgroundColor = 'hsl(228, 100%, 84%)';
+  document.getElementById('circle' + currentStep).style.color = 'hsl(213, 96%, 18%)';
 }
+
+function toggleDate(){
+  document.getElementsByClassName('monthly-toggle')
+}
+
